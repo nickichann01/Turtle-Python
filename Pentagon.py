16cbcd82966132673c8ff134f59c80a76c45de3d
Nyx <93776264+nickichann01@users.noreply.github.com>
2022-12-29 23:01:39 +0800

    Update and rename heptagon.py to Pentagon.py

--- Pentagon.py
+++ Pentagon.py
@@ -7,16 +7,16 @@ t = turtle.Turtle()
 # Set the turtle's pen size
 t.pensize(1)
 
-# Move the turtle to the starting position for the heptagon
+# Move the turtle to the starting position for the Pentagon
 t.penup()
 t.goto(-50, 0)
 t.pendown()
 
-# Draw a circle moving heptagon
+# Draw a circle moving Pentagon
 t.color('blue', 'purple')
 t.speed(0)
 
-#600 is for 600 heptagon
+#600 is for 600 Pentagon
 for i in range (600):
     if i % 5 == 0:
         t.lt(3)
